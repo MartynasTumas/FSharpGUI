1ab9551c2721d495a8cf3f26cfcb507f0b1098ff
Martynas <martynas.tumas@gmail.com>
2019-12-15 16:30:30 +0100

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at https://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open  System
+open System.Windows.Forms
+
+
+[<EntryPoint>]
+let main argv=
+    let form = new Form(Text = "F# form")
+    let button = new Button(Text = "Click me to close!", Dock = DockStyle.Fill)
+
+    button.Click.Add(fun _ -> Application.Exit() |> ignore)
+    form.Controls.Add(button)
+    // form.Show()
+    Application.Run(form)
+    0
+    
